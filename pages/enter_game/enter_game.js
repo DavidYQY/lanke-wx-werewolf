cc00735310258e9697bbd696287ef3a12e9ae076
Jingmei Hu <jingmei_hu@g.harvard.edu>
2021-02-16 22:34:30 -0500

    first try to get openid

--- pages/enter_game/enter_game.js
+++ pages/enter_game/enter_game.js
@@ -24,6 +24,14 @@ Page({
         })
       }
     })
+  },
+  async getOpenID() {
+    // 确认已经在 onLaunch 中调用过 wx.cloud.init 初始化环境
+    const res2 = wx.cloud.callContainer({
+      path: '/container-getopenid', // 填入容器的访问路径（云托管-服务列表-路径）
+      method: 'GET',
+    })
+    console.log(res2)
   }
  
 })
